c1fe80b74ff2244c7cc9b63a8787d8592b288500
Elen Shin <e.shin@joinposter.com>
2024-07-03 11:00:13 +0300

    pull main

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Category;
-use App\Models\Product;
-
-Route::get('/create-category', function () {
-    $category = Category::create(['name' => 'Electronics', 'slug' => 'electronics']);
-    return response()->json($category);
-});
-
-Route::get('/create-product', function () {
-    $category = Category::first(); // або знайдіть потрібну категорію іншим способом
-    $product = Product::create([
-        'name' => 'Smartphone',
-        'slug' => 'smartphone',
-        'SKU' => 'SP12345',
-        'description' => 'This is a sample smartphone.',
-        'price' => 499.99,
-        'discount' => 10,
-        'quantity' => 50,
-        'thumbnail' => 'path/to/thumbnail.jpg',
-        'category_id' => $category->id
-    ]);
-    return response()->json($product);
-});
-
-Route::get('/categories', function () {
-    return response()->json(Category::all());
-});
-
-Route::get('/products', function () {
-    return response()->json(Product::all());
-});
-
 
 /*
 |--------------------------------------------------------------------------
