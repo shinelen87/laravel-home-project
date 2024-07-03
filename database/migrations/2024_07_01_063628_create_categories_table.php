64647337c24010c1bc09af5613e20a27f8c13199
Elen Shin <e.shin@joinposter.com>
2024-07-03 10:47:38 +0300

    create factories and seeds

---
+++ database/migrations/2024_07_01_063628_create_categories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->string('name')->unique();
+            $table->bigInteger('parent_id')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->foreign('parent_id')->references('id')->on('categories')->onDelete('cascade');
+
+            $table->index(['parent_id']);
+            $table->fullText(['slug']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropForeign(['parent_id']);
+            $table->dropIndex(['parent_id']);
+            $table->dropFullText(['slug']);
+        });
+
+        Schema::dropIfExists('categories');
+    }
+};
